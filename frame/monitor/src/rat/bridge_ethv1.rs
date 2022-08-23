5976c6fe7fc8c166882089d5af2abd75f2b2e5df
fewensa <osuni@protonmail.com>
2022-08-24 00:47:06 +0800

    Use substorager

---
+++ frame/monitor/src/rat/bridge_ethv1.rs
@@ -0,0 +1,35 @@
+use crate::error::MonitorResult;
+use crate::traits::{AlertInfo, MonitorProbe};
+
+pub use self::types::*;
+
+/// bridge ethv1 probe
+pub struct BridgeEthv1Probe {
+  config: Ethv1ProbConfig,
+}
+
+impl BridgeEthv1Probe {
+  pub fn new(config: Ethv1ProbConfig) -> Self {
+    Self { config }
+  }
+}
+
+#[async_trait::async_trait]
+impl MonitorProbe for BridgeEthv1Probe {
+  async fn probe(&self) -> MonitorResult<AlertInfo> {
+    todo!()
+  }
+}
+
+impl BridgeEthv1Probe {
+  // async fn check
+}
+
+mod types {
+  use serde::{Deserialize, Serialize};
+
+  #[derive(Clone, Debug, Deserialize, Serialize)]
+  pub struct Ethv1ProbConfig {
+    pub endpoint: String,
+  }
+}
--- frame/monitor/src/traits/mod.rs
+++ frame/monitor/src/traits/mod.rs
@@ -1,3 +1,16 @@
+use crate::error::MonitorResult;
+
 /// monitor probe
 #[async_trait::async_trait]
-pub trait MonitorProbe {}
+pub trait MonitorProbe {
+  /// do probe
+  async fn probe(&self) -> MonitorResult<AlertInfo>;
+}
+
+/// probe data
+pub enum AlertInfo {
+  Normal,
+  P1,
+  P2,
+  P3,
+}
