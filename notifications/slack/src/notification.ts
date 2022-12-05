ec784141e6465b2a3c172a63de199c73eae849bd
fewensa <osuni@protonmail.com>
2022-12-05 14:54:48 +0800

    add slack webhook notification

--- notifications/slack/src/notification.ts
+++ notifications/slack/src/notification.ts
@@ -1,8 +1,26 @@
 import {Alert} from "alarmmgr-types";
 import {AlarmNotification} from "alarmmgr-notification-traits";
+import {IncomingWebhook} from "@slack/webhook";
 
 export class SlackNotification implements AlarmNotification {
-  async notify(alerts: Array<Alert>): Promise<void> {
 
+  private readonly webhook: IncomingWebhook;
+
+  constructor(
+    private readonly config: SlackWebhookConfig,
+  ) {
+    this.webhook = new IncomingWebhook(config.endpoint);
   }
+
+  async notify(alerts: Array<Alert>): Promise<void> {
+    await this.webhook.send({
+      username: 'Alarmmgr',
+      channel: '#darwinia-alert-notification',
+      text: 'I\'ve got news for you...',
+    });
+  }
+}
+
+export interface SlackWebhookConfig {
+  endpoint: string,
 }
