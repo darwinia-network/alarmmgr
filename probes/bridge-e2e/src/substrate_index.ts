cdba83aa19adb4d07ca9feed13b50dcff33d099f
nada <37610161+furoxr@users.noreply.github.com>
2023-01-11 17:13:35 +0800

    feat:support evm index

--- probes/bridge-e2e/src/substrate_index.ts
+++ probes/bridge-e2e/src/substrate_index.ts
@@ -1,40 +1,47 @@
 import fetch from 'node-fetch';
 
 export class SubstrateIndex {
-  host: string;
+  ecdsaIndexHost: string;
+  evmHost: string;
 
-  constructor(host: string) {
-    this.host = host;
+  constructor(ecdsaIndexHost: string, evmHost: string) {
+    this.ecdsaIndexHost = ecdsaIndexHost;
+    this.evmHost = evmHost;
   }
 
   async latestCollectingMessagesSignatures(): Promise<BasicEvent | null> {
-    const result: Response<CollectingNewMessagesSignatures> = await this.query(COLLECTING_NEW_MESSAGES_SIGNATURES, {});
+    const result: Response<CollectingNewMessagesSignatures> = await this.query(this.ecdsaIndexHost, COLLECTING_NEW_MESSAGES_SIGNATURES, {});
     return result.data.collectingNewMessageRootSignaturesEvents.nodes.pop() || null;
   }
 
   async latestCollectedMessageSignatures(): Promise<EnoughMessageSignaturesEvent | null> {
-    const result: Response<EnoughMessageSignatures> = await this.query(COLLECTED_ENOUGH_MESSAGES_SIGNATURES, {});
+    const result: Response<EnoughMessageSignatures> = await this.query(this.ecdsaIndexHost, COLLECTED_ENOUGH_MESSAGES_SIGNATURES, {});
     return result.data.collectedEnoughNewMessageRootSignaturesEvents.nodes.pop() || null;
   }
 
   async latestCollectingAuthoritiesChange(): Promise<BasicEvent | null> {
-    const result: Response<EnoughMessageSignatures> = await this.query(COLLECTING_NEW_AUTHORITIES_CHANGE, {});
+    const result: Response<EnoughMessageSignatures> = await this.query(this.ecdsaIndexHost, COLLECTING_NEW_AUTHORITIES_CHANGE, {});
     return result.data.collectedEnoughNewMessageRootSignaturesEvents.nodes.pop() || null;
   }
 
   async latestCollectedAuthoritiesChange(): Promise<BasicEvent | null> {
-    const result: Response<EnoughMessageSignatures> = await this.query(COLLECTED_AUTHORITIES_CHANGE, {});
+    const result: Response<EnoughMessageSignatures> = await this.query(this.ecdsaIndexHost, COLLECTED_AUTHORITIES_CHANGE, {});
     return result.data.collectedEnoughNewMessageRootSignaturesEvents.nodes.pop() || null;
   }
 
+  async message(nonce: number): Promise<MessageEvent | null> {
+    const result: Response<Messages> = await this.query(this.evmHost, MESSAGE, { nonce: nonce });
+    return result.data.messageAcceptedEntities.pop() || null;
+  }
 
-  async query<R>(query: string, variables: any): Promise<R> {
+
+  async query<R>(host: string, query: string, variables: any): Promise<R> {
     const body = {
       operationName: null,
       query,
       variables,
     };
-    const response = await fetch(this.host, {
+    const response = await fetch(host, {
       method: "post",
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
@@ -73,6 +80,13 @@ export type CollectedNewAuthoritiesChange = {
   collectedEnoughAuthoritiesChangeSignaturesEvents: {
     nodes: Array<BasicEvent>
   }
+}
+
+export type Messages = {
+  messageAcceptedEntities: Array<MessageEvent>
+}
+export type MessageEvent = {
+  block_number: number, nonce: number, encoded: string, source: string, target: string, id: string
 }
 
 const COLLECTING_NEW_MESSAGES_SIGNATURES: string = `
@@ -136,6 +150,23 @@ query nextCollectedEnoughAuthoritiesChangeSignaturesEven {
       blockNumber
       message
     }
+  }
+}
+`;
+
+const MESSAGE: string = `
+query LastTransaction($nonce: BigInt!) {
+  messageAcceptedEntities(
+    where: {
+        nonce: $nonce
+      }
+  ) {
+    block_number
+    nonce
+    encoded
+    source
+    target
+    id
   }
 }
 `;
