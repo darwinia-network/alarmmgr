5f878bed2eb981f546c6eaec25b470b069624732
nada <37610161+furoxr@users.noreply.github.com>
2023-01-18 18:39:19 +0800

    feat:register bridge-e2e probe

--- packages/bin/src/initializer.ts
+++ packages/bin/src/initializer.ts
@@ -1,11 +1,12 @@
-import {InstanceCenter} from "./plugins/probe_center";
-import {Bridge, BridgeS2SProbe} from "alarmmgr-probe-s2s";
-import {kvsEnvStorage} from "@kvs/env";
-import {KvsLocalStorage} from "@kvs/node-localstorage";
-import {KvStorageSchema} from "alarmmgr-types";
-import {SubstrateChainLiveProbe} from "alarmmgr-probe-chain-substrate/src";
-import {SlackNotification} from "alarmmgr-notification-slack";
-import {HttpProbe} from "alarmmgr-probe-http";
+import { InstanceCenter } from "./plugins/probe_center";
+import { Bridge, BridgeS2SProbe } from "alarmmgr-probe-s2s";
+import { kvsEnvStorage } from "@kvs/env";
+import { KvsLocalStorage } from "@kvs/node-localstorage";
+import { KvStorageSchema } from "alarmmgr-types";
+import { SubstrateChainLiveProbe } from "alarmmgr-probe-chain-substrate/src";
+import { SlackNotification } from "alarmmgr-notification-slack";
+import { HttpProbe } from "alarmmgr-probe-http";
+import { BridgeE2eProbe } from "alarmmgr-probe-e2e";
 
 
 export class Initializer {
@@ -19,23 +20,23 @@ export class Initializer {
   private static initProbeCenter(): void {
     // init s2s bridge probes
     const bridgeS2SProbes = [
-      {name: 'bridge-pangolin-pangoro', bridge: Bridge.PangolinPangoro},
-      {name: 'bridge-darwinia-crab', bridge: Bridge.DarwiniaCrab},
-      {name: 'bridge-pangolin-pangolinparachain', bridge: Bridge.PangolinPangolinParachain},
-      {name: 'bridge-crab-crabparachain', bridge: Bridge.CrabCrabParachain},
+      { name: 'bridge-pangolin-pangoro', bridge: Bridge.PangolinPangoro },
+      { name: 'bridge-darwinia-crab', bridge: Bridge.DarwiniaCrab },
+      { name: 'bridge-pangolin-pangolinparachain', bridge: Bridge.PangolinPangolinParachain },
+      { name: 'bridge-crab-crabparachain', bridge: Bridge.CrabCrabParachain },
     ];
     for (const probe of bridgeS2SProbes) {
-      InstanceCenter.registerProbe(probe.name, new BridgeS2SProbe({bridge: probe.bridge}));
+      InstanceCenter.registerProbe(probe.name, new BridgeS2SProbe({ bridge: probe.bridge }));
     }
 
     // init substrate chain
     const substrateChainProbes = [
-      {name: 'substrate-pangolin', endpoint: 'https://pangolin-rpc.darwinia.network'},
-      {name: 'substrate-pangoro', endpoint: 'https://pangoro-rpc.darwinia.network'},
-      {name: 'substrate-darwinia', endpoint: 'https://rpc.darwinia.network'},
-      {name: 'substrate-crab', endpoint: 'https://crab-rpc.darwinia.network'},
-      {name: 'substrate-crab-parachain', endpoint: 'https://crab-parachain-rpc.darwinia.network'},
-      {name: 'substrate-darwinia-parachain', endpoint: 'https://parachain-rpc.darwinia.network'},
+      { name: 'substrate-pangolin', endpoint: 'https://pangolin-rpc.darwinia.network' },
+      { name: 'substrate-pangoro', endpoint: 'https://pangoro-rpc.darwinia.network' },
+      { name: 'substrate-darwinia', endpoint: 'https://rpc.darwinia.network' },
+      { name: 'substrate-crab', endpoint: 'https://crab-rpc.darwinia.network' },
+      { name: 'substrate-crab-parachain', endpoint: 'https://crab-parachain-rpc.darwinia.network' },
+      { name: 'substrate-darwinia-parachain', endpoint: 'https://parachain-rpc.darwinia.network' },
     ];
     for (const probe of substrateChainProbes) {
       InstanceCenter.registerProbe(probe.name, new SubstrateChainLiveProbe(probe))
@@ -43,12 +44,15 @@ export class Initializer {
 
     // init http probes
     const httpProbes = [
-      {name: 'api-supply-ring', url: 'https://api.darwinia.network/supply/ring',},
-      {name: 'apps-darwinia', url: 'https://apps.darwinia.network',},
+      { name: 'api-supply-ring', url: 'https://api.darwinia.network/supply/ring', },
+      { name: 'apps-darwinia', url: 'https://apps.darwinia.network', },
     ];
     for (const probe of httpProbes) {
       InstanceCenter.registerProbe(probe.name, new HttpProbe(probe))
     }
+
+    const bridgeE2EProbes = new BridgeE2eProbe();
+    InstanceCenter.registerProbe("bridge-darwinia-ethereum", bridgeE2EProbes);
 
     // init notifications
     InstanceCenter.addNotifications([
