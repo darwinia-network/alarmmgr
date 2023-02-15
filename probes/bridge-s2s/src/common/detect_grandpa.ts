efeb420c831fd333ba6ceec45ab703db9f9b50f1
fewensa <37804932+fewensa@users.noreply.github.com>
2023-02-15 16:11:11 +0800

    Change max allow missing block to 500 (#15)

--- probes/bridge-s2s/src/common/detect_grandpa.ts
+++ probes/bridge-s2s/src/common/detect_grandpa.ts
@@ -23,7 +23,7 @@ export class S2SBridgeProbeDetectGrandpa {
     this.arg = options.arg;
     this.sourceSubql = options.sourceSubql;
     this.targetSubql = options.targetSubql;
-    this.maxAllowMissingBlock = 800;
+    this.maxAllowMissingBlock = 500;
     this.parachainBridge = options.parachainBridge ?? false;
     this.grandpaPalletName = options.grandpaPalletName;
   }
