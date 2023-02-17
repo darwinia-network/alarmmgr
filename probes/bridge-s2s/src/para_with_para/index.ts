0da3f868430163b07449e30370586432789b0f74
fewensa <osuni@protonmail.com>
2023-02-17 14:06:35 +0800

    Add para with para bridge probe

--- probes/bridge-s2s/src/para_with_para/index.ts
+++ probes/bridge-s2s/src/para_with_para/index.ts
@@ -1,1 +1,10 @@
+import {AlarmProbe} from "alarmmgr-probe-traits";
+import {Alert, Lifecycle} from "alarmmgr-types";
 
+
+export class ParaWithParaBridgeProbe implements AlarmProbe {
+  async probe(lifecycle: Lifecycle): Promise<Array<Alert>> {
+    return Promise.resolve(undefined);
+  }
+
+}
