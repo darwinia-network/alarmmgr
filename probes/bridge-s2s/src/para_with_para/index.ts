69d2dab9927c4b7df3d5860561b96db75097ef11
fewensa <37804932+fewensa@users.noreply.github.com>
2023-02-21 11:44:30 +0800

    Add para with para bridge probe (#18)
    * Add para with para bridge probe
    
    * Para with para arg
    
    * Add para with para bridge probe

--- probes/bridge-s2s/src/para_with_para/index.ts
+++ probes/bridge-s2s/src/para_with_para/index.ts
@@ -1,1 +1,84 @@
+import {AlarmProbe} from "alarmmgr-probe-traits";
+import {Alert, Lifecycle} from "alarmmgr-types";
+import {ParaWithParaArg} from "../types/inner";
+import {Subquery} from "alarmmgr-subquery";
+import {S2SBridgeProbeDetectGrandpa} from "../common/detect_grandpa";
+import {S2SBridgeProbeDetectMessage} from "../common/detect_message";
 
+
+export class ParaWithParaBridgeProbe implements AlarmProbe {
+
+
+  private readonly arg: ParaWithParaArg;
+  private readonly lifecycle: Lifecycle;
+  private readonly sourceParaSubql: Subquery;
+  private readonly sourceRelaySubql: Subquery;
+  private readonly targetParaSubql: Subquery;
+  private readonly targetRelaySubql: Subquery;
+
+  constructor(options: {
+    lifecycle: Lifecycle,
+    arg: ParaWithParaArg,
+  }) {
+    this.arg = options.arg;
+    this.lifecycle = options.lifecycle;
+    this.sourceParaSubql = new Subquery(this.arg.sourceParaChain.subql);
+    this.sourceRelaySubql = new Subquery(this.arg.sourceRelayChain.subql);
+    this.targetParaSubql = new Subquery(this.arg.targetParaChain.subql);
+    this.targetRelaySubql = new Subquery(this.arg.targetRelayChain.subql);
+  }
+
+  async probe(): Promise<Array<Alert>> {
+    const _alertsGrandpaSourceRelayToTargetPara = await new S2SBridgeProbeDetectGrandpa({
+      arg: {
+        sourceChain: this.arg.sourceRelayChain,
+        sourceClient: this.arg.sourceRelayClient,
+        targetChain: this.arg.targetParaChain,
+        targetClient: this.arg.targetParaClient,
+      },
+      sourceSubql: this.sourceRelaySubql,
+      targetSubql: this.targetParaSubql,
+      parachainBridge: true,
+      // @ts-ignore
+      grandpaPalletName: this.arg.targetParaChain.bridge_target[this.arg.sourceParaChain.bridge_chain_name].query_name.grandpa
+    }).detect();
+    const _alertsGrandpaTargetRelayToSourcePara = await new S2SBridgeProbeDetectGrandpa({
+      arg: {
+        sourceChain: this.arg.targetParaChain,
+        sourceClient: this.arg.targetParaClient,
+        targetChain: this.arg.sourceRelayChain,
+        targetClient: this.arg.sourceRelayClient,
+      },
+      sourceSubql: this.targetParaSubql,
+      targetSubql: this.sourceRelaySubql,
+      parachainBridge: true,
+      // @ts-ignore
+      grandpaPalletName: this.arg.sourceParaChain.bridge_target[this.arg.targetParaChain.bridge_chain_name].query_name.grandpa
+    }).detect();
+    const _alertsMessageSourceParaToTargetPara = await new S2SBridgeProbeDetectMessage({
+      arg: {
+        sourceChain: this.arg.sourceParaChain,
+        sourceClient: this.arg.sourceParaClient,
+        targetChain: this.arg.targetParaChain,
+        targetClient: this.arg.targetParaClient,
+      },
+      lifecycle: this.lifecycle,
+    }).detect();
+    const _alertsMessageTargetParaToSourcePara = await new S2SBridgeProbeDetectMessage({
+      arg: {
+        sourceChain: this.arg.targetParaChain,
+        sourceClient: this.arg.targetParaClient,
+        targetChain: this.arg.sourceParaChain,
+        targetClient: this.arg.sourceParaClient,
+      },
+      lifecycle: this.lifecycle,
+    }).detect();
+    return [
+      ..._alertsGrandpaSourceRelayToTargetPara,
+      ..._alertsGrandpaTargetRelayToSourcePara,
+      ..._alertsMessageSourceParaToTargetPara,
+      ..._alertsMessageTargetParaToSourcePara,
+    ];
+  }
+
+}
--- probes/bridge-s2s/src/probe.ts
+++ probes/bridge-s2s/src/probe.ts
@@ -2,9 +2,10 @@ import {AlarmProbe} from "alarmmgr-probe-traits";
 import {Bridge} from "./types/expose";
 import {SoloWithSoloBridgeProde} from "./solo_with_solo";
 import {Alert, BRIDGE_CHAIN_INFO, Lifecycle, S2SBridgeChain} from "alarmmgr-types";
-import {SoloWithParaArg, SoloWithSoloArg} from "./types/inner";
+import {ParaWithParaArg, SoloWithParaArg, SoloWithSoloArg} from "./types/inner";
 import {SoloWithParaBridgeProde} from "./solo_with_para";
 import {SubstrateClientInstance} from "alarmmgr-plugin-conn-substrate/src";
+import {ParaWithParaBridgeProbe} from "./para_with_para";
 
 
 export class BridgeS2SProbe implements AlarmProbe {
@@ -19,7 +20,6 @@ export class BridgeS2SProbe implements AlarmProbe {
 
   async probe(lifecycle: Lifecycle): Promise<Array<Alert>> {
     switch (this.bridge) {
-      case Bridge.PangolinPangoro:
       case Bridge.DarwiniaCrab:
         const arg_0 = await this.extractSoloWithSoloChainPairs();
         return await new SoloWithSoloBridgeProde({lifecycle, arg: arg_0}).probe();
@@ -28,6 +28,9 @@ export class BridgeS2SProbe implements AlarmProbe {
       case Bridge.PangolinPangolinParachainAlpha:
         const arg_1 = await this.extractSoloWithParaChainPairs();
         return await new SoloWithParaBridgeProde({lifecycle, arg: arg_1}).probe();
+      case Bridge.PangolinPangoro:
+        const arg_2 = await this.extractParaWithParaChainPairs();
+        return await new ParaWithParaBridgeProbe({lifecycle, arg: arg_2}).probe();
       default:
         return [];
     }
@@ -76,5 +79,37 @@ export class BridgeS2SProbe implements AlarmProbe {
       sourceClient,
       targetClient,
     }
+  }
+
+  private async extractParaWithParaChainPairs(): Promise<ParaWithParaArg> {
+    const [leftChainName, rightChainName] = this.bridge.split('-');
+    const [sourceParaChain, targetParaChain]: [S2SBridgeChain, S2SBridgeChain] = [
+      BRIDGE_CHAIN_INFO[leftChainName], BRIDGE_CHAIN_INFO[rightChainName]
+    ];
+    sourceParaChain.bridge_chain_name = leftChainName;
+    targetParaChain.bridge_chain_name = rightChainName;
+
+    // @ts-ignore
+    const sourceRelayChainName = targetParaChain.bridge_target[sourceParaChain.bridge_chain_name].relay_chain;
+    // @ts-ignore
+    const targetRelayChainName = sourceParaChain.bridge_target[targetParaChain.bridge_chain_name].relay_chain;
+    const sourceRelayChain: S2SBridgeChain = BRIDGE_CHAIN_INFO[sourceRelayChainName];
+    const targetRelayChain: S2SBridgeChain = BRIDGE_CHAIN_INFO[targetRelayChainName];
+    sourceRelayChain.bridge_chain_name = sourceRelayChainName;
+    targetRelayChain.bridge_chain_name = targetRelayChainName;
+    const sourceParaClient = await SubstrateClientInstance.instance(sourceParaChain.endpoint.http);
+    const targetParaClient = await SubstrateClientInstance.instance(targetParaChain.endpoint.http);
+    const sourceRelayClient = await SubstrateClientInstance.instance(sourceRelayChain.endpoint.http);
+    const targetRelayClient = await SubstrateClientInstance.instance(targetRelayChain.endpoint.http);
+    return {
+      sourceParaChain,
+      targetParaChain,
+      sourceRelayChain,
+      targetRelayChain,
+      sourceParaClient,
+      targetParaClient,
+      sourceRelayClient,
+      targetRelayClient,
+    };
   }
 }
